506386c5f76cd1472f6977be8160e25e37b89c65
Midhun Paul <midhunpaul396@gmail.com>
2024-10-10 18:57:31 -0400

    Globe Animations

--- getting-started-with-threejs-main/index.js
+++ getting-started-with-threejs-main/index.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
 
+import { OrbitControls } from "jsm/controls/OrbitControls.js";
 const w =  window.innerWidth;
 const h =  window.innerHeight;
 const renderer = new THREE.WebGLRenderer({antialias: true});
@@ -14,6 +15,9 @@ const  camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
 camera.position.z = 2;
 const scene = new THREE.Scene();
 
+const controls =  new OrbitControls(camera, renderer.domElement);
+controls.enableDamping = true;
+controls.dampingFactor = 0.03;
 const geo = new THREE.IcosahedronGeometry(1.0, 2);
 const mat = new THREE.MeshStandardMaterial({
     color : 0xccffffff,
@@ -28,13 +32,16 @@ const wireMat = new THREE.MeshBasicMaterial({
     wireframe: true
 });
 const wireMesh = new THREE.Mesh(geo, wireMat);
-scene.add(wireMesh);
+wireMesh.scale.setScalar(1.001);
+mesh.add(wireMesh);
 
-const hemiLight =  new THREE.HemisphereLight(0xffffff, 0x000000)
+const hemiLight =  new THREE.HemisphereLight(0x0099ff, 0xaa5500)
 scene.add(hemiLight);
 
 function animate(t = 0){
     requestAnimationFrame(animate);
+    // mesh.rotation.y = t * 0.0001;
     renderer.render(scene, camera);
+    controls.update();
 }
 animate();
